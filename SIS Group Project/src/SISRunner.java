370459a62eab66e7b39b00c2d8d7f819d55e226c
jeffer.ng <jeffer.ng@MU-C1-14.mullen.pvt.k12.co.us>
2019-02-04 08:33:38 -0700

    finished basic runner

--- SIS Group Project/src/SISRunner.java
+++ SIS Group Project/src/SISRunner.java
@@ -2,11 +2,40 @@ import java.util.*;
 
 public class SISRunner
 	{
-
+		static Scanner userInput = new Scanner(System.in);
+		
 		public static void main(String[] args)
 			{
+				boolean choosing = true;
+				System.out.println("Welcome to the Student Information System"
+						+ "\nWhat would you like to do?");
+				do
+					{
+				System.out.println("[1] Add or delete student"
+						+ "\n[2] Change student grades/schedule"
+						+ "\n[3] Sort students");
 				
-
+				int option = userInput.nextInt();
+				
+					if(option == 1)
+						{
+							choosing = false;
+						}
+					else if(option == 2)
+						{
+							choosing = false;
+						}
+					else if(option == 3)
+						{
+							choosing = false;
+						}
+					else
+						{
+							System.out.println("That was not a valid choice. Please choose again");
+						}
+				
+					}
+				while(choosing);
 			}
 
 	}
