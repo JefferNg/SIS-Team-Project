111f0b1070e3edb892ca74025e920c7ee4943703
jeffer.ng <jeffer.ng@MU-C1-14.mullen.pvt.k12.co.us>
2019-02-04 08:53:08 -0700

    created student object

---
+++ SIS Group Project/src/StudentInfo.java
@@ -0,0 +1,59 @@
+
+public class StudentInfo
+	{
+		private String name;
+		private String courses;
+		private String grade;
+		private double gpa;
+		
+		public StudentInfo(String n, String c, String gr, double gp)
+		{
+			name = n;
+			courses = c;
+			grade = gr;
+			gpa = gp;
+			
+		}
+
+		public String getName()
+			{
+				return name;
+			}
+
+		public void setName(String name)
+			{
+				this.name = name;
+			}
+
+		public String getCourses()
+			{
+				return courses;
+			}
+
+		public void setCourses(String courses)
+			{
+				this.courses = courses;
+			}
+
+		public String getGrade()
+			{
+				return grade;
+			}
+
+		public void setGrade(String grade)
+			{
+				this.grade = grade;
+			}
+
+		public double getGpa()
+			{
+				return gpa;
+			}
+
+		public void setGpa(double gpa)
+			{
+				this.gpa = gpa;
+			}
+		
+		
+	}
