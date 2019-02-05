84483ac71823f8a92d6f4474dbd63e08731870a4
Kexin.Yu <Kexin.Yu@MU-C1-19.mullen.pvt.k12.co.us>
2019-02-05 08:37:42 -0700

    coco change1

--- SIS Group Project/src/SISRunner.java
+++ SIS Group Project/src/SISRunner.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.File;
 
 public class SISRunner
 	{
@@ -6,7 +7,18 @@ public class SISRunner
 		public static void main(String[] args)
 			{
 				
-
+				
+				
+				Scanner ask = new Scanner(System.in);
+				System.out.println(" 1) Change Grade   2) Switch Classes");
+				int answer = ask.nextInt();
+				if(answer == 1) {
+					Scanner ask2 = new Scanner(System.in);
+					System.out.println(" Whose grade do you want to change?");
+					String answer2 = ask2.nextLine();
+					
+					
+				}
 			}
 
 	}
