8f2b8c4c9c6a22fd1337b1ff9a1ba3468f9269e6
jeffer.ng <jeffer.ng@MU-C1-14.mullen.pvt.k12.co.us>
2019-02-05 08:31:39 -0700

    added arraylist for students

--- SIS Group Project/src/SISRunner.java
+++ SIS Group Project/src/SISRunner.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+import java.text.*;
 
 public class SISRunner
 	{
@@ -19,16 +20,224 @@ public class SISRunner
 					String grade2 = file.next();
 					String course3 = file.next();
 					String grade3 = file.next();
+					double gpa = 0.0;
+					double temp = 0.0;
 					
+					switch(grade1)
+					{
+						case "A":
+								{
+									temp += 4.0;
+									break;
+								}
+						case "A-":
+								{
+									temp += 3.7;
+									break;
+								}
+						case "B+":
+								{
+									temp += 3.3;
+									break;
+								}
+						case "B":
+								{
+									temp += 3.0;
+									break;
+								}
+						case "B-":
+								{
+									temp += 2.7;
+									break;
+								}
+						case "C+":
+								{
+									temp += 2.3;
+									break;
+								}
+						case "C":
+								{
+									temp += 2.0;
+									break;
+								}
+						case "C-":
+								{
+									temp += 1.7;
+									break;
+								}
+						case "D+":
+								{
+									temp += 1.3;
+									break;
+								}
+						case "D":
+								{
+									temp += 1.0;
+									break;
+								}
+						case "D-":
+								{
+									temp += 0.7;
+									break;
+								}
+						case "F":
+								{
+									temp += 0.0;
+									break;
+								}
+						
+					}
+					
+					switch(grade2)
+					{
+						case "A":
+								{
+									temp += 4.0;
+									break;
+								}
+						case "A-":
+								{
+									temp += 3.7;
+									break;
+								}
+						case "B+":
+								{
+									temp += 3.3;
+									break;
+								}
+						case "B":
+								{
+									temp += 3.0;
+									break;
+								}
+						case "B-":
+								{
+									temp += 2.7;
+									break;
+								}
+						case "C+":
+								{
+									temp += 2.3;
+									break;
+								}
+						case "C":
+								{
+									temp += 2.0;
+									break;
+								}
+						case "C-":
+								{
+									temp += 1.7;
+									break;
+								}
+						case "D+":
+								{
+									temp += 1.3;
+									break;
+								}
+						case "D":
+								{
+									temp += 1.0;
+									break;
+								}
+						case "D-":
+								{
+									temp += 0.7;
+									break;
+								}
+						case "F":
+								{
+									temp += 0.0;
+									break;
+								}
+						
+					}
+					
+					switch(grade3)
+					{
+						case "A":
+								{
+									temp += 4.0;
+									break;
+								}
+						case "A-":
+								{
+									temp += 3.7;
+									break;
+								}
+						case "B+":
+								{
+									temp += 3.3;
+									break;
+								}
+						case "B":
+								{
+									temp += 3.0;
+									break;
+								}
+						case "B-":
+								{
+									temp += 2.7;
+									break;
+								}
+						case "C+":
+								{
+									temp += 2.3;
+									break;
+								}
+						case "C":
+								{
+									temp += 2.0;
+									break;
+								}
+						case "C-":
+								{
+									temp += 1.7;
+									break;
+								}
+						case "D+":
+								{
+									temp += 1.3;
+									break;
+								}
+						case "D":
+								{
+									temp += 1.0;
+									break;
+								}
+						case "D-":
+								{
+									temp += 0.7;
+									break;
+								}
+						case "F":
+								{
+									temp += 0.0;
+									break;
+								}
+						
+					}
+					
+//					Format decimal = new DecimalFormat("0.00");
+//					gpa = temp / 3;
+//					String GPA = decimal.format(gpa);
+					
+//					students.add(new StudentInfo(firstName + " " + lastName, course1, grade1, course2, grade2, course3, grade3, GPA));
 					
 				}
 			
 		}
 		
-		public static void main(String[] args)
+		public static void main(String[] args) throws IOException
 			{
 				
+				fillList();
+				mainMenu();
 				
+			}
+
+		private static void mainMenu()
+			{
 				boolean choosing = true;
 				System.out.println("Welcome to the Student Information System"
 						+ "\nWhat would you like to do?");
@@ -59,6 +268,8 @@ public class SISRunner
 				
 					}
 				while(choosing);
+				System.out.println(students.get(0).getGpa());
+				
 			}
 
 	}
--- SIS Group Project/src/StudentInfo.java
+++ SIS Group Project/src/StudentInfo.java
@@ -2,22 +2,86 @@
 public class StudentInfo
 	{
 		private String name;
-		private String courses;
-		private String grade;
-		private double gpa;
+		private String courses1;
+		private String grade1;
+		private String gpa;
 		private String course2;
 		private String course3;
 		private String grade2;
 		private String grade3;
 		
-		public StudentInfo(String n, String c, String gr, double gp)
+		public StudentInfo(String n, String c1, String g1, String c2, String g2, String c3, String g3, String gp)
 		{
 			name = n;
-			courses = c;
-			grade = gr;
+			courses1 = c1;
+			grade1 = g1;
+			course2 = c2;
+			grade2 = g2;
+			course3 = c3;
+			grade3 = g3;
 			gpa = gp;
 			
 		}
+
+		public String getCourses1()
+			{
+				return courses1;
+			}
+
+		public void setCourses1(String courses1)
+			{
+				this.courses1 = courses1;
+			}
+
+		public String getGrade1()
+			{
+				return grade1;
+			}
+
+		public void setGrade1(String grade1)
+			{
+				this.grade1 = grade1;
+			}
+
+		public String getCourse2()
+			{
+				return course2;
+			}
+
+		public void setCourse2(String course2)
+			{
+				this.course2 = course2;
+			}
+
+		public String getCourse3()
+			{
+				return course3;
+			}
+
+		public void setCourse3(String course3)
+			{
+				this.course3 = course3;
+			}
+
+		public String getGrade2()
+			{
+				return grade2;
+			}
+
+		public void setGrade2(String grade2)
+			{
+				this.grade2 = grade2;
+			}
+
+		public String getGrade3()
+			{
+				return grade3;
+			}
+
+		public void setGrade3(String grade3)
+			{
+				this.grade3 = grade3;
+			}
 
 		public String getName()
 			{
@@ -31,30 +95,30 @@ public class StudentInfo
 
 		public String getCourses()
 			{
-				return courses;
+				return courses1;
 			}
 
 		public void setCourses(String courses)
 			{
-				this.courses = courses;
+				this.courses1 = courses;
 			}
 
 		public String getGrade()
 			{
-				return grade;
+				return grade1;
 			}
 
 		public void setGrade(String grade)
 			{
-				this.grade = grade;
+				this.grade1 = grade;
 			}
 
-		public double getGpa()
+		public String getGpa()
 			{
 				return gpa;
 			}
 
-		public void setGpa(double gpa)
+		public void setGpa(String gpa)
 			{
 				this.gpa = gpa;
 			}
